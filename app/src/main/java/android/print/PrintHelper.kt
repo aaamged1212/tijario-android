1f8f54b2e3302752d954758ae21e05f96bc43077
aaamged1212 <aaamged1212@gmail.com>
2026-07-27 23:31:11 +0300

    fix(android): export documents as vector PDFs

---
+++ app/src/main/java/android/print/PrintHelper.kt
@@ -0,0 +1,39 @@
+package android.print
+
+import android.os.CancellationSignal
+import android.os.ParcelFileDescriptor
+
+/** Bridges package-private print callbacks so WebView writes a vector PDF file. */
+object PrintHelper {
+    fun runWrite(
+        adapter: PrintDocumentAdapter,
+        attributes: PrintAttributes,
+        pfd: ParcelFileDescriptor,
+        onComplete: () -> Unit,
+        onFailed: (String?) -> Unit,
+    ) {
+        adapter.onLayout(
+            null,
+            attributes,
+            CancellationSignal(),
+            object : PrintDocumentAdapter.LayoutResultCallback() {
+                override fun onLayoutFinished(info: PrintDocumentInfo?, changed: Boolean) {
+                    adapter.onWrite(
+                        arrayOf(PageRange.ALL_PAGES),
+                        pfd,
+                        CancellationSignal(),
+                        object : PrintDocumentAdapter.WriteResultCallback() {
+                            override fun onWriteFinished(pages: Array<out PageRange>?) = onComplete()
+                            override fun onWriteFailed(error: CharSequence?) = onFailed(error?.toString())
+                            override fun onWriteCancelled() = onFailed("Cancelled")
+                        },
+                    )
+                }
+
+                override fun onLayoutFailed(error: CharSequence?) = onFailed(error?.toString())
+                override fun onLayoutCancelled() = onFailed("Cancelled")
+            },
+            null,
+        )
+    }
+}
