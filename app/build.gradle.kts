d60a055cc50fd3ee4406f83b21f5be4946e75d5e
aaamged1212 <aaamged1212@gmail.com>
2026-07-31 06:51:30 +0300

    chore(android): release 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "app.tijario"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.1.6"
+        versionCode = 17
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
